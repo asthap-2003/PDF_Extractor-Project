438c2acd36a8f841e2f927dec14243b15eb031f1
astha suthar <asthap.yiion@gmail.com>
2025-08-25 18:54:25 +0530

    delete.py

--- delete.py
+++ delete.py
@@ -27,10 +27,10 @@ print("Extracted Text:\n", text)
 
 # MySQL Database Configuration
 db_config = {
- 'host': 'localhost',
-    'user': 'root',
-    'password': 'root',  # Same as in setup_database.py
-    'database': 'contract_data'
+     'host': 'localhost',
+    'user': 'gem',
+    'password': 'Y!!0n1z3#',  # Same as in setup_database.py
+    'database': 'gem'
 }
 
 def save_to_database(contract_id, filename, organisation_data, buyer_data, seller_data, products_list, total_order_value, text_format):
