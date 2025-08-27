3947c8a27ed45f709c9f27c7e2a77657a39206d0
astha <asthap.yiion@gmail.com>
2025-08-27 10:43:16 +0530

    debug remove

--- app.py
+++ app.py
@@ -4615,7 +4615,7 @@ def index():
     '''
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)   # debug=False for server
+    app.run(host='0.0.0.0', port=5000)   # debug=False for server
 
 
 
