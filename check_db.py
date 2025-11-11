9d93de88f1cdf2d448cea29b5669f4f7f4b790af
astha <asthap.yiion@gmail.com>
2025-11-11 14:27:18 +0530

    Add comment in the delete.py and check all db files

--- check_db.py
+++ check_db.py
@@ -1,13 +1,4 @@
 import mysql.connector
-
-# # MySQL Database Configuration
-# db_config = {
-#      'host': 'localhost',
-#     'user': 'gem',
-#     'password': 'Y!!0n1z3#',  # Same as in setup_database.py
-#     'database': 'gem'
-# }
-
 from db_config import db_config
 
 try:
